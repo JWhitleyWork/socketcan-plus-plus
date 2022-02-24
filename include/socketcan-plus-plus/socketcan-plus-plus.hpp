510ce65e833ea9391763a92e91019f919339879c
Joshua Whitley <whitleysoftwareservices@gmail.com>
2022-02-24 15:42:02 -0600

    Fixing install locations and uninstall script.

---
+++ include/socketcan-plus-plus/socketcan-plus-plus.hpp
@@ -0,0 +1,24 @@
+// Copyright 2022 Whitley Software Services
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SOCKETCAN_PLUS_PLUS__SOCKETCAN_HPP_
+#define SOCKETCAN_PLUS_PLUS__SOCKETCAN_HPP_
+
+#include "socketcan-plus-plus/socket_can_common.hpp"
+#include "socketcan-plus-plus/socket_can_id.hpp"
+#include "socketcan-plus-plus/socket_can_receiver.hpp"
+#include "socketcan-plus-plus/socket_can_sender.hpp"
+#include "socketcan-plus-plus/visibility_control.hpp"
+
+#endif  // SOCKETCAN_PLUS_PLUS__SOCKETCAN_HPP_
