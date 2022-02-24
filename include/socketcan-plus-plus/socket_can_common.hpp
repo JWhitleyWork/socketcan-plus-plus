3b1ab100ca2d813d793d7c78552dabcd0735255f
Joshua Whitley <whitleysoftwareservices@gmail.com>
2022-02-23 18:39:37 -0600

    Initial port from ros2_socketcan.

---
+++ include/socketcan-plus-plus/socket_can_common.hpp
@@ -0,0 +1,42 @@
+// Copyright 2021-2022 the Autoware Foundation, Whitley Software Services
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SOCKETCAN_PLUS_PLUS__SOCKET_CAN_COMMON_HPP_
+#define SOCKETCAN_PLUS_PLUS__SOCKET_CAN_COMMON_HPP_
+
+#include <sys/select.h>
+#include <sys/time.h>
+
+#include <chrono>
+#include <string>
+
+namespace socketcan
+{
+
+/// Bind a non-blocking CAN_RAW socket to the given interface
+/// \param[in] interface The name of the interface to bind, must be smaller than IFNAMSIZ
+/// \return The file descriptor bound to the given interface
+/// \throw std::runtime_error If one of socket(), fnctl(), ioctl(), bind() failed
+/// \throw std::domain_error If the provided interface name is too long
+int32_t bind_can_socket(const std::string & interface);
+/// Convert std::chrono duration to timeval (with microsecond resolution)
+struct timeval to_timeval(const std::chrono::nanoseconds timeout) noexcept;
+/// Convert timeval to time in microseconds
+uint64_t from_timeval(const struct timeval tv) noexcept;
+/// Create a fd_set for use with select() that only contains the specified file descriptor
+fd_set single_set(int32_t file_descriptor) noexcept;
+
+}  // namespace socketcan
+
+#endif  // SOCKETCAN_PLUS_PLUS__SOCKET_CAN_COMMON_HPP_
