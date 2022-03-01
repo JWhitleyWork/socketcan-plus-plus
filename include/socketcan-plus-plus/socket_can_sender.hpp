efe8cb64cc9bbf9c6d26e0c2a01fffe2ac20d764
Joshua Whitley <whitleysoftwareservices@gmail.com>
2022-03-01 15:47:56 -0600

    Update copyright for U Power.

--- include/socketcan-plus-plus/socket_can_sender.hpp
+++ include/socketcan-plus-plus/socket_can_sender.hpp
@@ -1,4 +1,4 @@
-// Copyright 2021-2022 the Autoware Foundation, Whitley Software Services
+// Copyright 2021-2022 the Autoware Foundation, U Power Robotics USA
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- include/socketcan-plus-plus/socketcan-plus-plus.hpp
+++ include/socketcan-plus-plus/socketcan-plus-plus.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Whitley Software Services
+// Copyright 2022 U Power Robotics USA
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
